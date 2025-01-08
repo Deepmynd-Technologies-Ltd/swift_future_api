de97662b1793ac4fb51587b31b0513b533ce5a3c
Sobilo34 <bilalsolih60@gmail.com>
2025-01-08 19:59:55 +0100

    update the build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 set -o errexit
-apt-get update 
-apt-get install -y libsystemd-dev pkg-config build-essential
-pip install -r requirements.txt
+# Redirect APT cache to a temporary directory
+export APT_CACHE_DIR=$(mktemp -d)
+sudo mkdir -p "$APT_CACHE_DIR/apt/lists/partial"
+sudo ln -sfT "$APT_CACHE_DIR/apt" /var/lib/apt
+
+# Update and install required system packages
+sudo apt-get update
+sudo apt-get install -y libsystemd-dev pkg-config build-essential
+pip install --only-binary :all: -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
