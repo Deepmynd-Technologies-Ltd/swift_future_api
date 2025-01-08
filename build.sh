e0cda4d90639df0737aaf3ed6024f9830fb10e7f
Sobilo34 <bilalsolih60@gmail.com>
2025-01-08 20:08:41 +0100

    update the build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -o errexit
-pip install --only-binary :all: -r requirements.txt
+pip install --no-deps -r requirements.txt || true
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
