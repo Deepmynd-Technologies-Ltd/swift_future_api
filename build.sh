05e62a41821a4d084dcbce116d6ddcfdc6816c2a
Sobilo34 <bilalsolih60@gmail.com>
2025-01-09 18:13:43 +0100

    Add true for validity

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ python manage.py collectstatic --no-input
 gunicorn core.wsgi --bind 0.0.0.0:8000
 
 
-if [[$CREATE_SUPERUSER]];
+if [[$CREATE_SUPERUSER == "true"]];
 then
     echo "Creating superuser"
     python manage.py createsuperuser --no-input
