738b9886d342619b505ec863806bc90ba9e5be3d
Sobilo34 <bilalsolih60@gmail.com>
2025-01-09 17:15:46 +0100

    create supersuser in render

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
 gunicorn core.wsgi --bind 0.0.0.0:8000
+
+
+if [[$CREATE_SUPERUSER]];
+then
+    echo "Creating superuser"
+    python manage.py createsuperuser --no-input
+fi
